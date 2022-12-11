8e8733dd70ea6fb6668877d8f6e25bb340b5d22e
André Menezes <1992menezes@gmail.com>
2022-12-11 02:36:24 -0300

    Fix Collapse margins

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Collapsible, Grid } from '@shopify/polaris'
+import { Button, Collapsible } from '@shopify/polaris'
 import Field from './Field'
 
 export default function Collapse({
@@ -10,34 +10,33 @@ export default function Collapse({
   const [ collapseIsOpen, setCollapseIsOpen ] = useState(false)
 
   return (
-    <Grid gap='5'>
-      <Grid.Cell columnSpan={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}>
-        <Button
-          ariaExpanded={collapseIsOpen}
-          ariaControls={field.id}
-          onClick={() => setCollapseIsOpen(!collapseIsOpen)}
-        >
-          {field.label}
-        </Button>
-      </Grid.Cell>
-      <Grid.Cell columnSpan={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}>
-        <Collapsible open={collapseIsOpen} id={field.id}>
-          <Grid gap='5'>
-            {Object.entries(field).map(property => (
-              <Grid.Cell
-                columnSpan={{ xs: 12, sm: 12, md: 12, lg: 12, xl: 12 }}
-              >
-                <Field
-                  chave={property[0]}
-                  value={property[1]}
-                  interfaceSectionInfos={interfaceSectionInfos}
-                  setInterfaceSectionInfos={setInterfaceSectionInfos}
-                />
-              </Grid.Cell>
-            ))}
-          </Grid>
-        </Collapsible>
-      </Grid.Cell>
-    </Grid>
+    <>
+      <Button
+        ariaExpanded={collapseIsOpen}
+        ariaControls={field.id}
+        onClick={() => setCollapseIsOpen(!collapseIsOpen)}
+      >
+        {field.label}
+      </Button>
+      {collapseIsOpen && (
+        <>
+          <br />
+          <br />
+        </>
+      )}
+      <Collapsible open={collapseIsOpen} id={field.id}>
+        {Object.entries(field).map(property => (
+          <>
+            <Field
+              chave={property[0]}
+              value={property[1]}
+              interfaceSectionInfos={interfaceSectionInfos}
+              setInterfaceSectionInfos={setInterfaceSectionInfos}
+            />
+            <br />
+          </>
+        ))}
+      </Collapsible>
+    </>
   )
 }
